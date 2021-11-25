0a30f6b7eee3f54b64f69dcc9dea286f4e8dde79
Anthony <ant.davi07@gmail.com>
2021-11-25 20:21:32 -0300

    python

---
+++ python/positovosNegativos.py
@@ -0,0 +1,16 @@
+#variavel que recebe um inteiro
+numero = int(input("Digite um número inteiro: "))
+
+#condições para verificar se o numero é positivo ou negativo
+
+#se for maior que zero é positivo
+if numero > 0:
+    print(numero, "é um número positivo")
+#se for menor que zero é negativo
+if numero < 0:
+    print(numero, "é um numero negativo")
+#caso o usuario digite 0
+if numero == 0:
+    print("Você digitou zero")
+
+
---
+++ python/seuNome.py
@@ -0,0 +1,6 @@
+#variavel com um input que recebe uma string com o nome do usuario
+seuNome = str(input("Digite seu nome: "))
+
+#print que exibe uma mensagem concatenada com o valor da variavel 
+print("Seu nome é: " + seuNome)
+
