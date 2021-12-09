0911c6de135ac22f32d2f0e0d8a9b8e755eccf41
Anthony <ant.davi07@gmail.com>
2021-12-09 18:51:25 -0300

    estudos

--- python/cadastro.py
+++ python/cadastro.py
@@ -1,56 +1,35 @@
-cadastro = []
+# Função de cadastro de nomes e idades
+def cadastrar(i_nome, i_idade):
+    
+    novo_cadastro = []
+    novo_cadastro.append(i_nome)
+    novo_cadastro.append(i_idade)
 
-cadastroNome1 = str(input("Qual o seu nome? "))
-cadastroIdade1 = int(input("Qual a sua idade? "))
+    cadastro.append(novo_cadastro)
 
-pessoa1 = []
-pessoa1.append(cadastroNome1)
-pessoa1.append(cadastroIdade1)
+# Função para exibir o cadastro
+def exibir():
 
-cadastro.append(pessoa1)
-print("\nPessoa cadastrada com sucesso!\n")
+    print("\nNomes cadastrados:\n")
 
-cadastroNome2 = str(input("Qual o seu nome? "))
-cadastroIdade2 = int(input("Qual a sua idade? "))
+    for x in range(len(cadastro)):
 
-pessoa2 = []
-pessoa2.append(cadastroNome2)
-pessoa2.append(cadastroIdade2)
+        nome = cadastro[x][0]
+        idade = cadastro[x][1]
+        print("Nome", x+1, ":", nome)
+        print("Idade", x+1, ":", idade)
 
-cadastro.append(pessoa2)
-print("\nPessoa cadastrada com sucesso!\n")
+# Função para solicitar as entradas
+def solicitar_nomes(qtde):
 
-cadastroNome3 = str(input("Qual o seu nome? "))
-cadastroIdade3 = int(input("Qual a sua idade? "))
+    for i in range(qtde):
+        nome = input("Informe o nome: ")
+        idade = int(input("Informe a idade: "))
 
-pessoa3 = []
-pessoa3.append(cadastroNome3)
-pessoa3.append(cadastroIdade3)
+        cadastrar(nome, idade)
 
-cadastro.append(pessoa3)
-print("\nPessoa cadastrada com sucesso!\n")
-
-cadastroNome4 = str(input("Qual o seu nome? "))
-cadastroIdade4 = int(input("Qual a sua idade? "))
-
-pessoa4 = []
-pessoa4.append(cadastroNome4)
-pessoa4.append(cadastroIdade4)
-
-cadastro.append(pessoa4)
-print("\nPessoa cadastrada com sucesso!\n")
-
-cadastroNome5 = str(input("Qual o seu nome? "))
-cadastroIdade5 = int(input("Qual a sua idade? "))
-
-pessoa5 = []
-pessoa5.append(cadastroNome5)
-pessoa5.append(cadastroIdade5)
-
-cadastro.append(pessoa5)
-print("\nPessoa cadastrada com sucesso!\n")
-
-print("Essas são as pessoas cadastradas: \n")
-
-for i in range(len(cadastro)):
-    print("\n", cadastro[i])
+# Rotina principal
+cadastro = [] # Declaração do array em branco
+quantidade = int(input("Quantos nomes você quer cadastrar? ")) # Pergunta quantos nomes deverão ser cadastrados
+solicitar_nomes(quantidade)
+exibir()
--- python/media.py
+++ python/media.py
@@ -1,18 +1,42 @@
-#as entradas são as notas dos alunos
-nota1 = int(input("Digite a nota do 1º semestre: "))
-nota2 = int(input("Digite a nota do 2º semestre: "))
-nota3 = int(input("Digite a nota do 3º semestre: "))
-nota4 = int(input("Digite a nota do 4º semestre: "))
+# Definição da função de cálculo de média
+def solicita_notas():
 
-print("\nAs notas dos alunos são: ", nota1, nota2, nota3, nota4)
+    todasAsNotas = []
+    
+    for i in range(4):
+        nota = int(input("Qual a nota do bimestre? "))
+        todasAsNotas.append(nota)
 
-#calculo de media
-media = ((nota1 + nota2 + nota3 + nota4) / 4)
+    return todasAsNotas
 
-print("\nA média do aluno é: ", media)
+def calcula_media(todasAsNotas):
 
-#condição de aprovação do aluno
-if media >= 5:
-    print("\nAluno aprovado!")
-else:
-    print("\nAluno reprovado!")
+    # Calcula a média
+    m = ( n1 + n2 + n3 + n4 ) / 4
+    return m
+
+def exibir_resultado():
+    # Exibe as notas e a média
+    print("\n") 
+    print("Nota do primeiro bimestre: ", nota1)
+    print("Nota do segundo bimestre: ", nota2)
+    print("Nota do terceiro bimestre: ", nota3)
+    print("Nota do quarto bimestre: ", nota4)
+    print("\n")
+    print("Média: ", media)
+
+    # Exibe o resultado aprovado ou reprovado
+    if (media >= 5):
+        print("Resultado: APROVADO")
+    else:
+        print("Resultado: REPROVADO")
+
+nota1 = 0
+nota2 = 0
+nota3 = 0
+nota4 = 0
+
+
+solicita_notas()
+# media = calcula_media(nota1, nota2, nota3, nota4)
+# exibir_resultado()
